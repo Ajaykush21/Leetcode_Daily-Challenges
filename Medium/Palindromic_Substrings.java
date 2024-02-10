c1ef1e6e49600a506ce47902d4160c1bed1d4314
Ajaykush21 <ajaykushwaha7310@gmail.com>
2024-02-10 14:47:41 +0530

    10 Feb 2k24

---
+++ Medium/Palindromic_Substrings.java
@@ -0,0 +1,22 @@
+package Medium;
+
+public class Palindromic_Substrings {
+	int result = 0;
+
+	public int countSubstrings(String s) {
+
+		for (int i = 0; i < s.length(); i++) {
+			count(s, i, i);
+			count(s, i, i + 1);
+		}
+		return result;
+	}
+
+	private void count(String s,int start,int end) {
+			while(start>=0 && end<s.length() && s.charAt(start)==s.charAt(end)) {
+				result++;
+				start--;
+				end++;
+	        
+	    }
+}
